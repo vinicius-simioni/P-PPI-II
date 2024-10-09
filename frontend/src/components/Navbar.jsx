383effa764e888151b27a244621941b9f98ca588
Vinícius Simioni <viniciussimioni@outlook.com>
2024-10-09 20:05:31 -0300

    create logout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   const token = localStorage.getItem('token');
 
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    window.location.reload();
+  };
+
   return (
     <nav className="bg-blue-500 text-white p-4 shadow-lg flex justify-between">
       <h1>Circular Livros</h1>
@@ -12,6 +17,7 @@ const Navbar = () => {
           <>
             <li className='mx-3'><Link to="/profile" className="hover:underline">Perfil</Link></li>
             <li className='mx-3'><Link to="/history" className="hover:underline">Histórico de Trocas</Link></li>
+            <li className='mx-3'><Link to="/" onClick={handleLogout} className="hover:underline">Logout</Link></li>
           </>
         ) : (
           <li className='mx-3'><Link to="/login" className="hover:underline">Login</Link></li>
