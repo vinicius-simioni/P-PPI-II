d2793c67b400f84512cd76da190dee0a17ab9934
Vinícius Simioni <viniciussimioni@outlook.com>
2024-10-12 12:36:52 -0300

    first landing page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,6 @@
-import BookList from '../components/BookList.jsx';
-import InterestBook from '../components/InterestBook.jsx';
-
 const Home = () => (
   <div className="text-center">
-    <h1 className="text-4xl font-bold mb-6 text-gray-800">Plataforma para troca de livros</h1>
-    <InterestBook />
-    <BookList />
+    <p>Landing Page - Plataforma para troca de livros</p>
   </div>
 );
 
