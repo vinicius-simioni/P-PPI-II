c0dc1de897100978754034d331690c574d34c0e6
Vinícius Simioni <viniciussimioni@outlook.com>
2024-10-13 21:50:58 -0300

    fix autoincrement

--- backend/models/Livro.js
+++ backend/models/Livro.js
@@ -5,6 +5,7 @@ module.exports = (sequelize, DataTypes) => {
       id: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        autoIncrement: true,
         primaryKey: true,
       },
       titulo: {
