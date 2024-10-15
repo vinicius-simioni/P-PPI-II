3b9c757826d16a820d2c3512cbde645f2fe496fc
Vinícius Simioni <viniciussimioni@outlook.com>
2024-10-14 22:26:21 -0300

    create findBookByUser

--- backend/controllers/LivroController.js
+++ backend/controllers/LivroController.js
@@ -1,4 +1,4 @@
-const { Livro } = require('../models');
+const { Livro } = require("../models");
 
 class LivroController {
   async index(req, res) {
@@ -6,6 +6,26 @@ class LivroController {
       const livros = await Livro.findAll();
       res.status(200).json(livros);
     } catch (error) {
+      res.status(500).json({ error: "Erro ao buscar livros" });
+    }
+  }
+
+  async findBookByUser(req, res) {
+    try {
+      // id do usuário extraído do middleware
+      const id_usuario = req.user_id;
+
+      const livros = await Livro.findAll({
+        where: { id_usuario: id_usuario },
+      });
+
+      if (livros.length === 0) {
+        return res.status(404).json({ message: 'Nenhum livro encontrado para este usuário.' });
+      }
+
+      res.status(200).json(livros);
+    } catch (error) {
+      console.error('Erro ao buscar livros:', error);
       res.status(500).json({ error: 'Erro ao buscar livros' });
     }
   }
@@ -15,11 +35,11 @@ class LivroController {
     try {
       const livro = await Livro.findByPk(id);
       if (!livro) {
-        return res.status(404).json({ error: 'Livro não encontrado' });
+        return res.status(404).json({ error: "Livro não encontrado" });
       }
       res.status(200).json(livro);
     } catch (error) {
-      res.status(500).json({ error: 'Erro ao buscar livro' });
+      res.status(500).json({ error: "Erro ao buscar livro" });
     }
   }
 
@@ -33,7 +53,7 @@ class LivroController {
       const novoLivro = await Livro.create({ titulo, autor, id_usuario });
       res.status(201).json(novoLivro);
     } catch (error) {
-      res.status(500).json({ error: 'Erro ao criar livro' });
+      res.status(500).json({ error: "Erro ao criar livro" });
     }
   }
 
@@ -43,7 +63,7 @@ class LivroController {
     try {
       const livro = await Livro.findByPk(id);
       if (!livro) {
-        return res.status(404).json({ error: 'Livro não encontrado' });
+        return res.status(404).json({ error: "Livro não encontrado" });
       }
       livro.titulo = titulo;
       livro.id_autor = id_autor;
@@ -51,7 +71,7 @@ class LivroController {
       await livro.save();
       res.status(200).json(livro);
     } catch (error) {
-      res.status(500).json({ error: 'Erro ao atualizar livro' });
+      res.status(500).json({ error: "Erro ao atualizar livro" });
     }
   }
 
@@ -60,12 +80,12 @@ class LivroController {
     try {
       const livro = await Livro.findByPk(id);
       if (!livro) {
-        return res.status(404).json({ error: 'Livro não encontrado' });
+        return res.status(404).json({ error: "Livro não encontrado" });
       }
       await livro.destroy();
       res.status(204).send();
     } catch (error) {
-      res.status(500).json({ error: 'Erro ao deletar livro' });
+      res.status(500).json({ error: "Erro ao deletar livro" });
     }
   }
 }
