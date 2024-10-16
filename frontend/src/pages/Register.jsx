b94a220980efec7875c3c342a980dc02618a5d87
Vinícius Simioni <viniciussimioni@outlook.com>
2024-10-16 20:02:55 -0300

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,36 +1,86 @@
 import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-const Register = () => {
+const Login = () => {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [senha, setSenha] = useState('');
+  const [cidade, setCidade] = useState('');
+  const [nome, setNome] = useState('');
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Register with:', { email, password });
+
+    axios.post("http://localhost:3000/api/register", {
+      nome: nome,
+      email: email,
+      senha: senha,
+      cidade: cidade
+    })
+      .then((response) => {
+        console.log('Login bem-sucedido:', response.data);
+
+        // guardar o token no localstorage
+        const token = response.data.token;
+        localStorage.setItem('token', token)
+
+        // redirecionar para o dashboard
+        navigate('/dashboard')
+      })
+      .catch((error) => {
+        console.error('Erro no login:', error);
+      });
   };
 
   return (
-    <div>
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
+    <div className="max-w-lg mx-auto mt-8">
+      <h2 className="text-2xl font-bold mb-6 text-center">Cadastre-se</h2>
+      <form onSubmit={handleSubmit} className="space-y-4">
+
+        <input
+          type="text"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          placeholder="Nome"
+          required
+          className="border p-2 w-full rounded-lg"
+        />
+
         <input
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder="Email"
           required
+          className="border p-2 w-full rounded-lg"
         />
         <input
           type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          placeholder="Senha"
           required
+          className="border p-2 w-full rounded-lg"
         />
-        <button type="submit">Cadastrar</button>
+
+        <input
+          type="text"
+          value={cidade}
+          onChange={(e) => setCidade(e.target.value)}
+          placeholder="Cidade"
+          required
+          className="border p-2 w-full rounded-lg"
+        />
+
+
+        <button type="submit" className="bg-blue-500 text-white p-2 w-full rounded-lg hover:bg-blue-700">
+          Cadastrar
+        </button>
+
       </form>
     </div>
   );
 };
 
-export default Register;
+export default Login;
