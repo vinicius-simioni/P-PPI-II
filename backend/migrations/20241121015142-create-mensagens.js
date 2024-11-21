239ae1335e07b9d736d60b0314eb00c7276c99da
Vinícius Simioni <viniciussimioni@outlook.com>
2024-11-20 23:29:37 -0300

    Create 20241121015142-create-mensagens.js

---
+++ backend/migrations/20241121015142-create-mensagens.js
@@ -0,0 +1,49 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('mensagens', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      texto: {
+        type: Sequelize.STRING(500),
+        allowNull: false,
+      },
+      id_emissor: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'usuarios',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      id_receptor: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'usuarios',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('mensagens');
+  },
+};
