2d47ed59dd33b8bdfaac23588ba0740784dc14e3
Vinícius Simioni <viniciussimioni@outlook.com>
2024-12-02 14:46:57 -0300

    Update router.js

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -6,10 +6,13 @@ const LivroController = require('../controllers/LivroController');
 const UsuarioController = require('../controllers/UsuarioController');
 const AvaliacaoController = require('../controllers/AvaliacaoController');
 const MensagemController = require("../controllers/MensagemController");
+const TrocaController = require("../controllers/TrocaController")
 
 // Rotas para autenticação
 router.route("/register").post(UsuarioController.create);
 router.route("/login").post(UsuarioController.login);
+router.put('/usuarios', verifyTokenMiddleware, UsuarioController.update);
+router.get('/usuarios/me', verifyTokenMiddleware, UsuarioController.getById);
 
 // Rotas para livros
 router.get('/livros', verifyTokenMiddleware, LivroController.index);
@@ -29,6 +32,9 @@ router.get("/mensagens", verifyTokenMiddleware, MensagemController.listarMensage
 router.get('/mensagens/:id', verifyTokenMiddleware, MensagemController.historicoMensagens);
 router.get('/chats', verifyTokenMiddleware, MensagemController.listarChats);
 
-
+// Rotas para troca
+router.post("/trocas", TrocaController.criarTroca);
+router.get("/trocas", TrocaController.listarTrocas);
+router.get("/trocas/:id", TrocaController.detalhesTroca);
 
 module.exports = router;
