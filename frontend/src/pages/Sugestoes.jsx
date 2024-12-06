bc61f173e0a220466e3ea5f7cb22cee68ae76e26
Vinícius Simioni <viniciussimioni@outlook.com>
2024-12-06 14:22:49 -0300

    Update Sugestoes.jsx

--- frontend/src/pages/Sugestoes.jsx
+++ frontend/src/pages/Sugestoes.jsx
@@ -196,7 +196,7 @@ const Sugestoes = () => {
                   <select
                     className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm bg-white"
                     value={livroProposto}
-                    onChange={(e) => setLivroProposto(e.target.value)} // Atualiza o estado com o valor selecionado
+                    onChange={(e) => setLivroProposto(e.target.value)}
                     required
                   >
                     <option value="">Selecione um livro</option>
@@ -218,7 +218,7 @@ const Sugestoes = () => {
                   <input
                     type="date"
                     value={dataTroca} // Associando ao estado
-                    onChange={(e) => setDataTroca(e.target.value)} // Atualiza o estado com a data selecionada
+                    onChange={(e) => setDataTroca(e.target.value)}
                     className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm bg-white"
                     required
                   />
