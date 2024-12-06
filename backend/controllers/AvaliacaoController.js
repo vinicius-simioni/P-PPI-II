449cf08a52656b4289e50a29d986a94056d372ee
Vinícius Simioni <viniciussimioni@outlook.com>
2024-12-06 15:11:04 -0300

    Update AvaliacaoController.js

--- backend/controllers/AvaliacaoController.js
+++ backend/controllers/AvaliacaoController.js
@@ -1,43 +1,53 @@
-const { Avaliacao, Usuario } = require('../models');
+const { Avaliacao, Usuario } = require("../models");
 
 class AvaliacaoController {
   async criarAvaliacao(req, res) {
-    const { id_usuario_avaliado, nota, comentario } = req.body;
-    const id_usuario_avaliador = req.user_id; // Pega o ID do usuário autenticado
-
-    if (id_usuario_avaliado === id_usuario_avaliador) {
-      return res.status(400).json({ error: "Você não pode se autoavaliar." });
-    }
-
     try {
-      // Criação da avaliação
-      await Avaliacao.create({
+      const id_usuario_avaliador = req.user_id;
+      const { nota, comentario } = req.body;
+      const id_usuario_avaliado = req.params.id;
+
+      console.log(id_usuario_avaliado, id_usuario_avaliador, nota, comentario)
+
+      // Verificar se os usuários existem
+      const usuarioAvaliado = await Usuario.findByPk(id_usuario_avaliado);
+      const usuarioAvaliador = await Usuario.findByPk(id_usuario_avaliador);
+
+      if (!usuarioAvaliado || !usuarioAvaliador) {
+        return res.status(400).json({ error: "Usuários não encontrados" });
+      }
+
+      // Criar avaliação
+      const avaliacao = await Avaliacao.create({
         id_usuario_avaliado,
         id_usuario_avaliador,
         nota,
         comentario,
       });
 
-      // Recalcula a reputação média do usuário avaliado
-      const avaliacoes = await Avaliacao.findAll({
-        where: { id_usuario_avaliado },
-        attributes: ["nota"],
-      });
-
-      const reputacaoMedia =
-        avaliacoes.reduce((total, avaliacao) => total + avaliacao.nota, 0) /
-        avaliacoes.length;
-
-      // Atualiza a reputação no perfil do usuário avaliado
-      await Usuario.update(
-        { reputacao: reputacaoMedia.toFixed(2) },
-        { where: { id: id_usuario_avaliado } }
-      );
-
-      res.status(201).json({ message: "Avaliação criada com sucesso!" });
+      return res.status(201).json(avaliacao);
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: "Erro ao criar a avaliação." });
+      return res.status(500).json({ error: "Erro ao cadastrar avaliação" });
+    }
+  }
+
+  async listarAvaliacoes(req, res) {
+    try {
+      const { id } = req.params;
+
+      const avaliacoes = await Avaliacao.findAll({
+        where: { id_usuario_avaliado: id },
+        include: [
+          { model: Usuario, as: "avaliado", attributes: ["nome"] },
+          { model: Usuario, as: "avaliador", attributes: ["nome"] },
+        ],
+      });
+
+      return res.status(200).json(avaliacoes);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Erro ao listar avaliações" });
     }
   }
 }
